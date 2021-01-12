269f7aef85e40003a7d0307c494778afe393be7d
Antonio Neto <antonionetobhz@gmail.com>
2021-01-12 19:59:08 -0300

    corretion

--- desafio11.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW cancoes_premium AS
-SELECT music.title AS `nome`, COUNT(u_m.music_id) AS `reproducoes` FROM users_musics AS u_m
-INNER JOIN musics AS music ON u_m.music_id = music.music_id
-INNER JOIN users AS user ON u_m.user_id = user.user_id
-WHERE user.plan_id IN (2,3)
-GROUP BY music.music_id
-ORDER BY music.title;
--- desafio6.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW faturamento_atual AS
-SELECT MIN(plan.value) AS `faturamento_minimo`,
-MAX(plan.value) AS `faturamento_maximo`,
-ROUND(AVG(plan.value), 2) AS `faturamento_medio`,
-SUM(plan.value) AS `faturamento_total`
-FROM users AS user 
-JOIN plans AS plan ON user.plan_id = plan.plan_id;
