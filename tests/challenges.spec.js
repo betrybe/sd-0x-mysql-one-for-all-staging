1e97327031a039bc45f398592b1dc21c8730f2f0
nato-re <alvares.renato@hotmail.com>
2021-02-04 16:18:33 -0300

    test his project

--- tests/challenges.spec.js
+++ tests/challenges.spec.js
@@ -14,6 +14,7 @@ describe('Queries de seleção', () => {
       await importer.import('./desafio1.sql');
     }
     catch(error) {
+      console.log(error)
       console.log('Erro ao restaurar o dump!');
     }
 
